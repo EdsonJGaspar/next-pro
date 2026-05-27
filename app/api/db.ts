e4d14e57ca89565bdf1615ebf04819f6600fab3c
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-05-27 09:24:07 +0100

    feat: Criando rotas API no Next.js

---
+++ app/api/db.ts
@@ -0,0 +1,11 @@
+const books = [
+  {
+    id: 1,
+    title: "O homem mais rico da BabilÔnia",
+    author: "George S. Clason",
+  },
+  { id: 2, title: "Pai rico, pai pobre", author: "Robert Kiyosaki" },
+  { id: 3, title: "O milagre da manhã", author: "Hal Elrod" },
+];
+
+export default books;
