115f734a045b15aaff925c5c229375d5755f2f78
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-05-27 08:37:11 +0100

    feat: Rotas dinamicas

--- app/(dashboard)/dashboard/usuarios/page.tsx
+++ app/(dashboard)/dashboard/usuarios/page.tsx
@@ -1,26 +1,46 @@
 import Link from "next/link";
 
-export default function UsuariosPage() {
+interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+  };
+}
+[];
+
+export default async function UsuariosPage() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!response.ok) {
+    throw new Error("Falha na busca dos usuários");
+  }
+  const users: UserProps[] = await response.json();
+
   return (
     <main>
       <h1>Pagina de usuários</h1>
 
-      <div className="mt-10">
+      <section className="mt-10">
         <ul>
-          <li>
-            <Link href={"/dashboard/usuarios/1"}>Usuário 1</Link>{" "}
-          </li>
-          <li>
-            <Link href={"/dashboard/usuarios/2"}>Usuário 2</Link>{" "}
-          </li>
-          <li>
-            <Link href={"/dashboard/usuarios/3"}>Usuário 3</Link>{" "}
-          </li>
-          <li>
-            <Link href={"/dashboard/usuarios/4"}>Usuário 4</Link>{" "}
-          </li>
+          {users.map((user) => {
+            return (
+              <li key={user.id}>
+                <Link href={`/dashboard/usuarios/${user.id}`}>{user.name}</Link>
+              </li>
+            );
+          })}
         </ul>
-      </div>
+      </section>
     </main>
   );
 }
