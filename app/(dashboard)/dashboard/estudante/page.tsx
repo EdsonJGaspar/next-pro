1dc46dcfafef1b8fe5ad1f805ef826a7e5037adb
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-09-01 10:31:32 +0100

    update: Ajustando formulario

--- app/(dashboard)/dashboard/estudante/page.tsx
+++ app/(dashboard)/dashboard/estudante/page.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useForm } from "@tanstack/react-form-nextjs";
 import { FieldInfo } from "./fiel-info";
+import { Button } from "@/components/ui/button";
 
 export default function EstudantePage() {
   const form = useForm({
@@ -12,6 +13,7 @@ export default function EstudantePage() {
     },
     onSubmit: async ({ value }) => {
       console.log(value);
+      form.reset();
     },
   });
   return (
@@ -25,12 +27,12 @@ export default function EstudantePage() {
         }}
         className="max-w-2xl mx-auto"
       >
-        <>
+        <div>
           <form.Field
             name="firstNmae"
             validators={{
               onChange: ({ value }) =>
-                !value
+                !value.trim()
                   ? "Campo obrigatório"
                   : value.length < 3
                     ? "Primeiro nome deve ter no minimo 3 caracteres"
@@ -38,10 +40,7 @@ export default function EstudantePage() {
               onChangeAsyncDebounceMs: 500,
               onChangeAsync: async ({ value }) => {
                 await new Promise((resolve) => setTimeout(resolve, 1000));
-                return (
-                  value.includes("error") &&
-                  'Nenhum "error" no campo Primeiro nome'
-                );
+                return value.includes("error") && "Erro no Primeiro nome";
               },
             }}
             children={(field) => {
@@ -61,7 +60,45 @@ export default function EstudantePage() {
               );
             }}
           />
-        </>
+        </div>
+        <div>
+          <form.Field
+            name="lastName"
+            children={(field) => {
+              return (
+                <div>
+                  <Label htmlFor={field.name}>Ultimo Nome</Label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                </div>
+              );
+            }}
+          />
+        </div>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <>
+              <Button type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "Enviando..." : "Enviar"}
+              </Button>
+              <Button
+                type="reset"
+                onClick={(e) => {
+                  e.preventDefault();
+                  form.reset();
+                }}
+              >
+                Reset
+              </Button>
+            </>
+          )}
+        />
       </form>
     </div>
   );
